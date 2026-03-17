3265ac1984e627d14dec3ff22a0dab58f8cc7f7f
Brian Stout <bstout@ara.com>
2026-03-17 11:50:13 -0400

    Some binding changes, tree-sitter

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,4 +8,17 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map('n', '<Leader>rc', ':cclose<CR>', { desc = "Close quick window for lsp.ref", noremap = true, silent = true })
 
+vim.api.nvim_create_user_command('DiagnosticsToggleVirtualText', function()
+    local current_value = vim.diagnostic.config().virtual_text
+    if current_value then
+        vim.diagnostic.config({ virtual_text = false })
+        print("Virtual text disabled")
+    -- else
+        vim.diagnostic.config({ virtual_text = true })
+        print("Virtual text enabled")
+    end
+end, {})
+
+vim.keymap.set("n", "<leader>dv", ":DiagnosticsToggleVirtualText<cr>", { desc = "Toggle LSP virtual text" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
